63ba25086b4ad4bfc86c327f081e18aeffe7587f
Davidov Murad <muradavud@gmail.com>
2021-03-10 18:51:54 +0100

    h2 setup

---
+++ src/main/java/com/muradavud/ufcquiz/ufcquiz/UfcQuizApplication.java
@@ -0,0 +1,13 @@
+package com.muradavud.ufcquiz.ufcquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muradavud/ufcquiz/ufcquiz/UfcQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muradavud.ufcquiz.ufcquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
