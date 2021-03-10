d3c22e908710353a53f8341f69375d25113beaa6
Davidov Murad <muradavud@gmail.com>
2021-03-10 22:43:42 +0100

    fighter image scrape service

--- src/main/java/com/muradavud/ufcquiz/ufcquiz/UfcQuizApplication.java
+++ src/main/java/com/muradavud/ufcquiz/ufcquiz/UfcQuizApplication.java
@@ -1,13 +1,27 @@
 package com.muradavud.ufcquiz.ufcquiz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UfcQuizApplication {
+public class UfcQuizApplication implements CommandLineRunner
+{
+
+	@Autowired
+	private FighterImageService fighterImageScrapeService;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(UfcQuizApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("HERE -> {}", fighterImageScrapeService.scrapeImageUrl("Ovince Saint Preux"));
+	}
 }
